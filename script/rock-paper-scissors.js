bef1bfaae25b15d3f11262ab8ed8947ef5a2c771
Giorgi Garsevanishvili <george.garsevanidi@gmail.com>
2024-04-07 17:25:42 +0300

    RPS-v7

--- script/rock-paper-scissors.js
+++ script/rock-paper-scissors.js
@@ -29,25 +29,25 @@ document.querySelector('.auto-play').addEventListener('click', () => {
 });
 
 document.querySelector('.js-rock-button').addEventListener('click', () => {
-  playGame('Rock');
+  playGame('rock');
 });
 
 document.querySelector('.js-paper-button').addEventListener('click', () => {
-  playGame('Paper');
+  playGame('paper');
 });
 
 document.querySelector('.js-scissors-button').addEventListener('click', () => {
-  playGame('Scissors');
+  playGame('scissors');
 });
 
 
 document.body.addEventListener('keydown', (event) => {
   if (event.key === 'r') {
-  playGame('Rock');
+  playGame('rock');
   } else if (event.key === 'p') {
-    playGame('Paper');
+    playGame('paper');
   } else if (event.key === 's'){
-    playGame('Scissors');
+    playGame('scissors');
   }
 })
 
@@ -82,32 +82,32 @@ const computerMove = pickComputerMove();
 
 let result  = '';
 
-if (playerMove === 'Scissors'){
-    if (computerMove === 'Rock'){
+if (playerMove === 'scissors'){
+    if (computerMove === 'rock'){
     result = 'You lose.'
-  } else if ( computerMove === 'Paper'){
+  } else if ( computerMove === 'paper'){
     result = 'You win.'
-  } else if (computerMove === 'Scissors')
+  } else if (computerMove === 'scissors')
   {
     result = 'Tie.'
   }
   
-} else if (playerMove === 'Rock'){
-    if (computerMove === 'Rock'){
+} else if (playerMove === 'rock'){
+    if (computerMove === 'rock'){
     result = 'Tie.'
-  } else if ( computerMove === 'Paper'){
+  } else if ( computerMove === 'paper'){
     result = 'You lose.'
-  } else if (computerMove === 'Scissors')
+  } else if (computerMove === 'scissors')
   {
     result = 'You win.'
   }
 
-} else if (playerMove === 'Paper'){
-  if (computerMove === 'Rock'){
+} else if (playerMove === 'paper'){
+  if (computerMove === 'rock'){
     result = 'You win.'
-  } else if ( computerMove === 'Paper'){
+  } else if ( computerMove === 'paper'){
     result = 'Tie.'
-  } else if (computerMove === 'Scissors')
+  } else if (computerMove === 'scissors')
   {
     result = 'You lose.'
   }
@@ -129,7 +129,7 @@ document.querySelector('.js-result')
   .innerHTML = result;
 
 document.querySelector('.js-moves')
-  .innerHTML = `You <img src="images /${playerMove}-emoji.png" class= "move-icon"> <img src="images /${computerMove}-emoji.png" class= "move-icon" > Computer`;
+  .innerHTML = `You <img src="images/${playerMove}-emoji.png" class= "move-icon"> <img src="images/${computerMove}-emoji.png" class= "move-icon" > Computer`;
 
 }
 
@@ -147,11 +147,11 @@ const randomNumber = Math.random();
 let computerMove = '';
 
 if (randomNumber >= 0 && randomNumber < 1/3 ) {
-computerMove = ('Rock');
+computerMove = ('rock');
 } else if (randomNumber >= 1/3 && randomNumber <= 2/3) 
-{computerMove = ('Paper');
+{computerMove = ('paper');
 } else if (randomNumber >= 2/3 && randomNumber <1){
-computerMove = ('Scissors');
+computerMove = ('scissors');
 }
 
 return computerMove;
